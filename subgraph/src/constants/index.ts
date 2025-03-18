db8b8cf881d98967765a56d8e1580fa396d1d647
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2025-03-18 18:20:28 +0200

    refactor: move constants one level
    feat: subgraph constants

---
+++ subgraph/src/constants/index.ts
@@ -0,0 +1,6 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+
+export let zero = BigInt.fromI32(0);
+export let one = BigInt.fromI32(1);
+export let two = BigInt.fromI32(2);
+export let d18 = BigInt.fromI32(18);
