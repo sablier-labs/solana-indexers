7c7429d8b386f04f3735b12de27ca63a8100ede5
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2025-04-02 15:46:12 +0300

    feat: implement support for anchor events in the substream

--- src/idl/mod.rs
+++ src/idl/mod.rs
@@ -1,5 +1,5 @@
 pub mod idl {
     use anchor_lang::declare_program;
 
-    declare_program!(program);
+    declare_program!(lockup_linear_v10);
 }
--- subgraph/src/adapters.ts
+++ subgraph/src/adapters.ts
@@ -1,7 +1,6 @@
 export { Data as ProtoData } from "../generated/substreams/v1/program/Data";
 export { Cancel as EventCancel } from "../generated/substreams/v1/program/Cancel";
 export { CreateWithTimestamps as EventCreateWithTimestamps } from "../generated/substreams/v1/program/CreateWithTimestamps";
-export { Initialize as EventInitialize } from "../generated/substreams/v1/program/Initialize";
 export { Renounce as EventRenounce } from "../generated/substreams/v1/program/Renounce";
 export { Withdraw as EventWithdraw } from "../generated/substreams/v1/program/Withdraw";
 export { WithdrawMax as EventWithdrawMax } from "../generated/substreams/v1/program/WithdrawMax";
