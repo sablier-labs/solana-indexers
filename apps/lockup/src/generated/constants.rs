b8fc7a4e5f11f2cfe5e6e83640f24a3aaf766b4c
Razvan Gabriel <razvan.gabriel.apostu@gmail.com>
2025-06-09 10:11:06 +0300

    refactor: reorganize repo as workspace, both rust and ts
    feat: create shared packages

---
+++ apps/lockup/src/generated/constants.rs
@@ -0,0 +1,5 @@
+// @generated
+
+pub mod cluster {
+ pub const SABLIER_LOCKUP_LINEAR_V10: [&str; 1] = ["DczGzCFQ5kHRBGVkH22HDdtduE4qaqopdc5HGLrAzfQD",];
+}
---
+++ apps/lockup/subgraph/generated/env.ts
@@ -0,0 +1,8 @@
+export let chainId = 900000020;    
+export let chainCode = "SOLDEV";    
+export let cluster = "devnet";
+export let substream = "solana-devnet";
+
+export let startBlock_lockup = 384182200;
+
+export let linear: string[][] = [["DczGzCFQ5kHRBGVkH22HDdtduE4qaqopdc5HGLrAzfQD", "LL", "V10"],];
